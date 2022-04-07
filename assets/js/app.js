b282a6621482a37d0717a109ac102f688c80b7e4
franmp <franmontecinos.p@gmail.com>
2022-04-06 21:51:17 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+let numJuegos = parseInt(prompt("Ingrese la cantidad de veces que desea jugar"));
+if(numJuegos>0){
+    document.write("Juguemos");
+}
+else{
+    alert("Por favor, ingrese una respuesta válida. Recargue la página para reintentarlo.");
+}
